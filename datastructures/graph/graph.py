2422a575cf6890c19dd3986c02b7a0fd3f052dfc
Benjamin Paothatat <bpaothat@gmail.com>
2022-07-05 18:32:44 -0700

    Add graph implementation

---
+++ datastructures/graph/graph.py
@@ -0,0 +1,49 @@
+class Graph:
+    def __init__(self):
+        self._graph_dict = {}
+
+    def edges(self, vertice):
+        return self._graph_dict[vertice]
+
+    def all_vertices(self):
+        return self._graph_dict.keys()
+
+    def all_edges(self):
+        return self.__generate_edges()
+
+    def add_vertix(self, vertex):
+        if vertex not in self._graph_dict:
+            self._graph_dict[vertex] = []
+
+    def add_edge(self, edge):
+        edge = set(edge)
+        vertex1, vertex2 = tuple(edge)
+        for x, y in [(vertex1, vertex2), (vertex2, vertex1)]:
+            if x in self._graph_dict:
+                self._graph_dict[x].add(y)
+            else:
+                self._graph_dict[x] = [y]
+
+    def __generate_edges(self):
+        edges = []
+        for vertex in self._graph_dict:
+            for neighbour in self._graph_dict[vertex]:
+                if {neighbour, vertex} not in edges:
+                    edges.append({vertex, neighbour})
+        return edges
+    
+    def __iter__(self):
+        self._iter_obj = iter(self._graph_dict)
+        return self._iter_obj
+    
+    def __next__(self):
+        return next(self._iter_obj)
+
+    def __str__(self):
+        res = "vertices: "
+        for k in self._graph_dict:
+            res += str(k) + " "
+        res += "\nedges: "
+        for edge in self.__generate_edges():
+            res += str(edge) + " "
+        return res

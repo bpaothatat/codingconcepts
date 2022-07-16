c45c452a5d1be4f5eda6338f526bd2f03862ed10
Benjamin Paothatat <bpaothat@gmail.com>
2022-07-16 15:41:27 -0700

    Add BT in order traversal and build tree method

--- datastructures/tree/binarytree.py
+++ datastructures/tree/binarytree.py
@@ -1,3 +1,6 @@
+from logging import root
+
+
 class BinaryTree:
     def __init__(self, data) -> None:
         self._data = data
@@ -5,16 +8,33 @@ class BinaryTree:
         self._right = None
 
     def insert(self, data) -> None:
-        if self.data:
-            if data < self.data:
+        if self._data:
+            if data < self._data:
                 if self._left is None:
                     self._left = BinaryTree(data)
                 else:
                     self._left.insert(data)
-            elif data > self.data:
+            elif data > self._data:
                 if self._right is None:
                     self._right = BinaryTree(data)
                 else:
                     self._right.insert(data)
         else:
-            self.data = data
+            self._data = data
+
+    def in_order_traversal(self):
+        items = []
+        if self._left:
+            items += self._left.in_order_traversal()
+        items.append(self._data)
+        if self._right:
+            items += self._right.in_order_traversal()
+        return items
+
+def build_tree(data:list) -> BinaryTree:
+    root = None
+    if len(data) > 0:
+        root = BinaryTree(data[0])
+        for i in range(1, len(data)):
+            root.insert(data[i]) 
+    return root

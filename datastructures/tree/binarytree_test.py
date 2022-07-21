b01a7592ce5abbcbcf6cf5cb6c538993a2c6e036
Benjamin Paothatat <bpaothat@gmail.com>
2022-07-20 19:34:39 -0700

    Add BT test for min and max function

--- datastructures/tree/binarytree_test.py
+++ datastructures/tree/binarytree_test.py
@@ -7,6 +7,14 @@ class BinaryTreeTest(unittest.TestCase):
         tree = build_tree([17, 4, 1, 1, 10])
         self.assertEqual(tree.in_order_traversal(), [1, 4, 10, 17])
 
+    def test_min(self):
+        tree = build_tree([3,1,10])
+        self.assertEqual(tree.find_min(), 1)
+
+    def test_max(self):
+        tree = build_tree([3,1,10])
+        self.assertEqual(tree.find_max(), 10)
+
     def test_search(self):
         tree = build_tree([17, 4, 1, 1, 10])
         self.assertTrue(tree.search(17))
